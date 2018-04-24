5ceea25331ed4e513f1efa54b6051dbf48c0b032
hemanth <hemanthdevarapalli92@gmail.com>
2018-04-24 17:45:03 -0500

    add admin interface

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,2 +1,20 @@
 ActiveAdmin.register User do
+
+permit_params :email, :admin
+    form do |f|
+        f.semantic_errors
+        f.inputs do
+            f.input :email
+            f.input :admin 
+        end
+    f.actions
+end
+
+    index do
+        id_column
+        column :email
+        column :admin
+        actions
+    end
+
 end
